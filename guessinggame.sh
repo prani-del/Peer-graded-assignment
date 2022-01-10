0eae81faa0dca616a2d489df405fc790314b6d89
prani-del <96740695+prani-del@users.noreply.github.com>
2022-01-10 17:48:59 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,10 +1,16 @@
 guess()
 {
+
 read guess
-if [ $guess -eq $(find . -ls|wc -l) ]
+
+if [ $guess -eq $(ls|wc -l) ]
 then
 echo "Your guess is absolutely correct...CONGRATULATIONS!!"
-elif [ $guess -gt $(find . -ls|wc -l) ]
+fi
+
+while[ $guess -ne $(ls|wc -l) ]
+do
+if [ $guess -gt $(ls|wc -l) ]
 then
 echo "Your guess is too high ,please try once again...."
 guess
@@ -12,9 +18,12 @@ else
 echo "Your guess is too low ,please try once again..."
 guess
 fi
+done
 }
 echo "Are you ready to play the guessing game?"
+
 echo "How many files are there in current directory?"
+
 guess
 
 
